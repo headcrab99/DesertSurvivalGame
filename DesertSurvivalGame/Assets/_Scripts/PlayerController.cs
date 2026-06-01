1b4e0b3f4680b9683f611c5e2ff1e5193863dacc
headcrab99 <yura.r.gab@gmail.com>
2026-06-01 23:29:33 +0300

    v02 Basic setup
    Added some textures and changed project settings, added new script Player Controller

---
+++ DesertSurvivalGame/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float hor;
+    private float ver;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
